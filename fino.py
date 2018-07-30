2f5a2b3e84cb89fa2e8a45e0cb28cff003744f9e
finosivithomas <41885671+finosivithomas@users.noreply.github.com>
2018-07-30 10:44:22 +0530

    Add files via upload

---
+++ fino.py
@@ -0,0 +1,13 @@
+l=[1,2,"hello",[1,2],3,2]
+#print(l)
+#print(l.count(2)) #occurence of the element 2 in list
+R=(1,2,"Hello")
+#print(R[0:1]) #cannot append the values
+S=(3,2,4,1,7,9)
+h=[4,2,6,1,7]
+h.sort() #
+h.sort(reverse=True) #sort the elements in reverse order
+#print(h)
+F={'apple','banana','mango'}
+#print(F) prints in sorted manner
+
